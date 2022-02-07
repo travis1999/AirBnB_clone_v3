dc6e5777dbbdccd501ca8af0c48c3a02681e27dc
travis1999 <trevoroguna@gmail.com>
2022-02-08 00:58:35 +0300

    app: added CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from flask import Flask
+# -*- coding: utf-8 -*
+# -
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 import os
 
-
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["0.0.0.0"]}})
 app.register_blueprint(app_views)
 
 
@@ -19,7 +21,15 @@ def on_exit(context):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """
+    This function is called when the application encounters a 404 error.
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port,threaded=True)
+    app.run(host=host, port=port, threaded=True)
