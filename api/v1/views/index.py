9bb831b71e0e87ac1aa122ebc6679bd9f3b5df18
travis1999 <trevoroguna@gmail.com>
2022-02-08 01:25:58 +0300

    added function documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify
 from models import storage
 
 
 @app_views.route('/status')
 def index():
+    """
+    returns the status of the API
+    """
     return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats')
 def stats():
+    """
+    returns the number of objects in the storage
+    """
     return jsonify({key: storage.count(value) for key, value in storage.models().items()})
