538f6aa0f255f254a4f15755bcc07323f5badc4d
travis1999 <trevoroguna@gmail.com>
2022-02-08 01:00:11 +0300

    api/v1: added a stats route
    the "/stats" route returns a json object with the count of each model
    as key value pairs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,14 @@
 
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status')
 def index():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def stats():
+    return jsonify({key: storage.count(value) for key, value in storage.models().items()})
